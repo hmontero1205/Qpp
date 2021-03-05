93aa32a5bb509bba1f7257cca6ef41309d313126
Hans Montero <hjm2133@columbia.edu>
2021-03-05 17:04:31 -0500

    Remove index from redirect route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :office_hours
   mount ActionCable.server => '/cable'
-  root :to => redirect('/office_hours/index')
+  root :to => redirect('/office_hours')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
