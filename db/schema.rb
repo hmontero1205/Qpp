75ea0f06a4d61b2df8c070952a9fe6029b767d86
Evan Mesterhazy <etm2131@columbia.edu>
2021-03-12 20:54:30 -0500

    Upgrade to Rails 6.1.3 and Ruby 2.7+ (#2)

--- db/schema.rb
+++ db/schema.rb
@@ -2,78 +2,78 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210311152217) do
+ActiveRecord::Schema.define(version: 2021_03_11_152217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", id: :bigserial, force: :cascade do |t|
-    t.string   "name",        null: false
-    t.string   "record_type", null: false
-    t.bigint   "record_id",   null: false
-    t.bigint   "blob_id",     null: false
-    t.datetime "created_at",  null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id", using: :btree
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true, using: :btree
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", id: :bigserial, force: :cascade do |t|
-    t.string   "key",          null: false
-    t.string   "filename",     null: false
-    t.string   "content_type"
-    t.text     "metadata"
-    t.string   "service_name", null: false
-    t.bigint   "byte_size",    null: false
-    t.string   "checksum",     null: false
-    t.datetime "created_at",   null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true, using: :btree
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", id: :bigserial, force: :cascade do |t|
-    t.bigint "blob_id",          null: false
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true, using: :btree
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "office_hours", force: :cascade do |t|
-    t.string   "host"
-    t.string   "class_name"
+  create_table "office_hours", id: :serial, force: :cascade do |t|
+    t.text "host"
+    t.text "class_name"
     t.datetime "time"
-    t.string   "zoom_info"
+    t.text "zoom_info"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "email"
+    t.string "email"
   end
 
-  create_table "queue_entries", force: :cascade do |t|
-    t.text     "student"
+  create_table "queue_entries", id: :serial, force: :cascade do |t|
+    t.text "student"
     t.datetime "start_time"
-    t.text     "description"
-    t.integer  "oh_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text "description"
+    t.integer "oh_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "name"
-    t.string   "surname"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "surname"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
