4dc9edc4e89f4a0c42dd9a0c125c75ad1d83054c
Hans Montero <hjm2133@columbia.edu>
2021-03-26 16:55:06 -0400

    Implement simple chat (#13)
    * Inject oh id from rails into JS
    
    * Create Chat model
    
    * Refactor show page to use grid
    
    * More HTML reworking
    
    * Implement chat
    
    * Delete chats when queue entry is deleted
    
    * Show thread count

--- app/channels/office_hour_channel.rb
+++ app/channels/office_hour_channel.rb
@@ -21,10 +21,19 @@ class OfficeHourChannel < ApplicationCable::Channel
       qe = QueueEntry.find(data["qeID"])
       if connection.current_user && qe.office_hour.user == connection.current_user
         QueueEntry.destroy(data["qeID"])
+        Chat.where(queue_entry_id: data["qeID"]).delete_all
         ActionCable.server.broadcast "office_hour_channel", "ohID": data["ohID"], op: "dequeue", qeID: data["qeID"]
       end
     elsif data["op"] == "refresh"
       ActionCable.server.broadcast "office_hour_channel", op: "refresh"
+    elsif data["op"] == "show_thread"
+      queue_entry = QueueEntry.find(data["qeID"]).as_json
+      chats = Chat.where(queue_entry_id: data["qeID"]).as_json
+      ActionCable.server.broadcast "office_hour_channel", op: "show_thread", ohID: data["ohID"], qeID: data["qeID"], qe: queue_entry, chats: chats, windowID: data["windowID"]
+    elsif data["op"] == "send_msg"
+      chat = Chat.create!("name": data["name"], "msg": data["msg"], "office_hour_id": data["ohID"], "queue_entry_id": data["qeID"])
+      num_chats = Chat.where(queue_entry_id: data["qeID"]).length
+      ActionCable.server.broadcast "office_hour_channel", op: "send_msg", ohID: data["ohID"], qeID: data["qeID"], chat: chat, num_chats: num_chats
     else
       # Unimplemented
     end
